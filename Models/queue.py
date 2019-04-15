2c6e370a676d52b1b224f9370ad60bae9d3e5f81
Vu Vo <vuv@uoregon.edu>
2019-04-14 21:37:22 -0700

    queue model in progress

--- Models/queue.py
+++ Models/queue.py
@@ -1,8 +1,22 @@
 '''
 CIS 422 Project 1
-Contributors: Jerry Xie
+Contributors: Jerry Xie, Vu Vo
 '''
+from collections import deque
 class Queue:
     studentQ = []
-    def __init__(self):
+    def __init__(self,studentQ):
+        stqueue = deque(studentQ)
         pass
+
+    def pop(self):
+        return stqueue.popleft()
+
+    def push(self,student):
+        return stqueue.append(student)
+
+    def popfrom(self,location):
+        #this function will remove a student from a specified location
+
+    def pushRandom(self,student):
+        #this function will randomly reinsert a student back into queue 
